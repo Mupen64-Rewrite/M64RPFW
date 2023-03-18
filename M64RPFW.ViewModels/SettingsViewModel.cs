6a6fadace74251de33078ae02d5dc1d03c06b433
Alex <48759429+Aurumaker72@users.noreply.github.com>
2023-03-18 20:42:21 +0100

    Implement settings

---
+++ M64RPFW.ViewModels/SettingsViewModel.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using M64RPFW.Services;
+using M64RPFW.ViewModels.Messages;
+
+namespace M64RPFW.ViewModels;
+
+
+public sealed partial class SettingsViewModel : ObservableObject, IRecipient<RomLoadingMessage>
+{
+    
+    public SettingsViewModel(IFilesService filesService, ILocalSettingsService localSettingsService)
+    {
+        _filesService = filesService;
+        _localSettingsService = localSettingsService;
+        WeakReferenceMessenger.Default.RegisterAll(this);
+    }
+    
+    private readonly IFilesService _filesService;
+    private readonly ILocalSettingsService _localSettingsService;
+
+    #region Properties
+    public ObservableCollection<string> RecentRomPaths
+    {
+        get => _localSettingsService.Get<ObservableCollection<string>>(nameof(RecentRomPaths));
+        set => SetSettingsProperty(nameof(RecentRomPaths), value);
+    }
+
+    public string[] RomExtensions
+    {
+        get => _localSettingsService.Get<string[]>(nameof(RomExtensions));
+        set => SetSettingsProperty(nameof(RomExtensions), value);
+    }
+    
+    public string CoreLibraryPath
+    {
+        get => _localSettingsService.Get<string>(nameof(CoreLibraryPath));
+        set => SetSettingsProperty(nameof(CoreLibraryPath), value);
+    }
+    
+    public string VideoPluginPath
+    {
+        get => _localSettingsService.Get<string>(nameof(VideoPluginPath));
+        set => SetSettingsProperty(nameof(VideoPluginPath), value);
+    }
+    
+    public string AudioPluginPath
+    {
+        get => _localSettingsService.Get<string>(nameof(AudioPluginPath));
+        set => SetSettingsProperty(nameof(AudioPluginPath), value);
+    }
+    
+    public string InputPluginPath
+    {
+        get => _localSettingsService.Get<string>(nameof(InputPluginPath));
+        set => SetSettingsProperty(nameof(InputPluginPath), value);
+    }
+    
+    public string RspPluginPath
+    {
+        get => _localSettingsService.Get<string>(nameof(RspPluginPath));
+        set => SetSettingsProperty(nameof(RspPluginPath), value);
+    }
+    
+    public int CoreType
+    {
+        get => _localSettingsService.Get<int>(nameof(CoreType));
+        set => SetSettingsProperty(nameof(CoreType), value);
+    }
+    
+    public int ScreenWidth
+    {
+        get => _localSettingsService.Get<int>(nameof(ScreenWidth));
+        set => SetSettingsProperty(nameof(ScreenWidth), value);
+    }
+    
+    public int ScreenHeight
+    {
+        get => _localSettingsService.Get<int>(nameof(ScreenHeight));
+        set => SetSettingsProperty(nameof(ScreenHeight), value);
+    }
+
+    public bool IsStatusbarVisible
+    {
+        get => _localSettingsService.Get<bool>(nameof(IsStatusbarVisible));
+        set => SetSettingsProperty(nameof(IsStatusbarVisible), value);
+    }
+    
+    public string Culture
+    {
+        get => _localSettingsService.Get<string>(nameof(Culture));
+        set => SetSettingsProperty(nameof(Culture), value);
+    }
+    
+    public string Theme
+    {
+        get => _localSettingsService.Get<string>(nameof(Theme));
+        set => SetSettingsProperty(nameof(Theme), value);
+    }
+    
+    #endregion
+
+    [RelayCommand]
+    private void RemoveRecentRomPath(string path)
+    {
+        RecentRomPaths.Remove(path);
+    } 
+
+    void IRecipient<RomLoadingMessage>.Receive(RomLoadingMessage message)
+    {
+        RecentRomPaths.Remove(message.Value);
+        RecentRomPaths.Insert(0, message.Value);
+    }
+    
+    private void SetSettingsProperty<T>(string key, T value, [CallerMemberName] string? callerMemberName = null)
+    {
+        ArgumentNullException.ThrowIfNull(callerMemberName);
+        _localSettingsService.Set(key, value);
+        OnPropertyChanged(callerMemberName);
+    }
+    
+   
+
+    private async Task<(bool Succeeded, string Path)> ShowLibraryFileDialog()
+    {
+        var file = await _filesService.TryPickOpenFileAsync(new[] { "dll" });
+        return file != null ? (true, file.Path) : ((bool Succeeded, string Path))(false, null);
+    }
+
+    [RelayCommand]
+    private async Task BrowseLibraryPath(string key)
+    {
+        var file = await _filesService.TryPickOpenFileAsync(new[] { "dll" });
+        if (file != null)
+        {
+            _localSettingsService.Set(key, file.Path);
+        }
+        else
+        {
+            // we failed
+            // TODO: maybe notify the user? not an important failure though
+        }
+    }
+
+    
+}
--- Mupen64PlusRR/Views/MainWindow.axaml.cs
+++ Mupen64PlusRR/Views/MainWindow.axaml.cs
@@ -1,10 +1,14 @@
+using System.ComponentModel;
+using System.IO;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using M64RPFW.Services;
 using M64RPFW.ViewModels;
 using Microsoft.Extensions.DependencyInjection;
 using Mupen64PlusRR.Controls;
+using Mupen64PlusRR.Services;
 
 namespace Mupen64PlusRR.Views;
 
@@ -13,16 +17,26 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         AvaloniaXamlLoader.Load(this);
-        
+
         var serviceCollection = ((App)Application.Current!).ServiceProvider.GetService<IServiceCollection>();
-        
+
         serviceCollection.AddSingleton<IOpenGLContextService>(this.Find<VidextControl>("EmulatorWindow"));
-        
+
         ((App)Application.Current!).ServiceProvider = serviceCollection.BuildServiceProvider();
-        
+
         DataContext = ((App)Application.Current!).ServiceProvider.GetService<MainViewModel>().EmulatorViewModel;
-        
     }
 
 
+    private void ShowSettingsDialogMenuItem_OnClick(object? sender, RoutedEventArgs e)
+    {
+        new SettingsDialog().ShowDialog(this);
+    }
+
+    private void Window_OnClosing(object? sender, CancelEventArgs e)
+    {
+        File.WriteAllText(App.LocalSettingsPath,
+            (((App)Application.Current!).ServiceProvider.GetService<ILocalSettingsService>() as LocalSettings)
+            .ToJson());
+    }
 }
