393a0de6ddbcf12983d3dc04837aadbd0cc31eb9
Alex <48759429+Aurumaker72@users.noreply.github.com>
2023-03-18 17:49:57 +0100

    Move all services into M64RPFW.Services project, utilize neutral naming

--- M64RPFW.Models/Interfaces/IVideoExtensionService.cs
+++ M64RPFW.Models/Interfaces/IVideoExtensionService.cs
@@ -1,32 +1,9 @@
 using System.Runtime.InteropServices;
 using M64RPFW.Models.Emulation;
 using M64RPFW.Models.Types;
+using M64RPFW.Services;
 
 namespace M64RPFW.Models.Interfaces;
-
-public unsafe interface IVideoExtensionService
-{
-    Mupen64PlusTypes.Error VidextInit();
-    Mupen64PlusTypes.Error VidextQuit();
-    
-    Mupen64PlusTypes.Error VidextListFullscreenModes(Span<Mupen64PlusTypes.Size2D> sizes, ref int len);
-    Mupen64PlusTypes.Error VidextListFullscreenRates(Mupen64PlusTypes.Size2D size, Span<int> output, ref int len);
-    
-    Mupen64PlusTypes.Error VidextSetVideoMode(int width, int height, int bpp, Mupen64PlusTypes.VideoMode mode, Mupen64PlusTypes.VideoFlags flags);
-    Mupen64PlusTypes.Error VidextSetVideoModeWithRate(int width, int height, int refreshRate, int bpp,
-        Mupen64PlusTypes.VideoMode mode, Mupen64PlusTypes.VideoFlags flags);
-
-    IntPtr VidextGLGetProcAddress(byte* symbol);
-    Mupen64PlusTypes.Error VidextGLSetAttr(Mupen64PlusTypes.GLAttribute attr, int value);
-    Mupen64PlusTypes.Error VidextGLGetAttr(Mupen64PlusTypes.GLAttribute attr, out int value);
-
-    Mupen64PlusTypes.Error VidextResizeWindow(int width, int height);
-    Mupen64PlusTypes.Error VidextSetCaption(string str);
-    Mupen64PlusTypes.Error VidextToggleFullscreen();
-    
-    Mupen64PlusTypes.Error VidextSwapBuffers();
-    uint VidextGLGetDefaultFramebuffer();
-}
 
 public static class VideoExtensionServiceExtensions
 {
