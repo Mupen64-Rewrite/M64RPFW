93d2b31c33d04c7d80ea8d5b2220734f328b1793
jgcodes2020 <jackyguo18@hotmail.com>
2023-04-20 14:57:53 -0400

    Custom markup for adding localized text

--- M64RPFW.Views.Avalonia/MarkupExtensions/LocalizationSource.cs
+++ M64RPFW.Views.Avalonia/MarkupExtensions/LocalizationSource.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Globalization;
 using System.Resources;
 using System.Threading;
@@ -24,6 +26,55 @@ public class LocalizationSource : INotifyPropertyChanged
             var @event = PropertyChanged;
             @event?.Invoke(this, new PropertyChangedEventArgs(string.Empty));
         }
+    }
+
+    private class ObservableSource : IObservable<string>
+    {
+        public ObservableSource(LocalizationSource parent, string key)
+        {
+            _parent = parent;
+            _observers = new HashSet<IObserver<string>>();
+            _key = key;
+
+            _parent.PropertyChanged += (_, _) =>
+            {
+                var value = _parent[_key];
+                foreach (var obs in _observers)
+                    obs.OnNext(value);
+            };
+        }
+
+        private class Disposer : IDisposable
+        {
+            public Disposer(ObservableSource parent, IObserver<string> observer)
+            {
+                _parent = parent;
+                _observer = observer;
+                _parent._observers.Add(observer);
+            }
+            
+            public void Dispose()
+            {
+                _parent._observers.Remove(_observer);
+            }
+            
+            private ObservableSource _parent;
+            private IObserver<string> _observer;
+        }
+        
+        public IDisposable Subscribe(IObserver<string> observer)
+        {
+            return new Disposer(this, observer);
+        }
+
+        private LocalizationSource _parent;
+        private HashSet<IObserver<string>> _observers;
+        private string _key;
+    }
+
+    public IObservable<string> GetObservable(string key)
+    {
+        return new ObservableSource(this, key);
     }
 
 
