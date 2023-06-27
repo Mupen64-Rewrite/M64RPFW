1cb1d088c1326d82e7a34d6c57b6f9531176768d
jgcodes2020 <jackyguo18@hotmail.com>
2023-06-26 21:28:16 -0400

    update to Avalonia 11.0.0-rc1.1

--- M64RPFW.Views.Avalonia/Controls/ClickableRadioButton.cs
+++ M64RPFW.Views.Avalonia/Controls/ClickableRadioButton.cs
@@ -8,9 +8,9 @@ namespace M64RPFW.Views.Avalonia.Controls;
 /// Part of a kludge for setting the savestate slots. Opens up the
 /// OnClick handler so that it may be manually called.
 /// </summary>
-public class ClickableRadioButton : RadioButton, IStyleable
+public class ClickableRadioButton : RadioButton
 {
-    public Type StyleKey => typeof(RadioButton);
+    public new Type StyleKeyOverride => typeof(RadioButton);
     
     /// <summary>
     /// Manually clicks the RadioButton.
