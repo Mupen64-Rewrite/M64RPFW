43aafe05628c4477ed6b2ce4ad3f40948cd8e140
jgcodes2020 <jackyguo18@hotmail.com>
2023-07-19 12:33:10 -0400

    Update APIs, add memory read/write API

---
+++ M64RPFW.Models/Emulation/RDRAM_API.cs
@@ -0,0 +1,10 @@
+using System.Runtime.InteropServices;
+using M64RPFW.Models.Helpers;
+using M64RPFW.Models.Types;
+
+namespace M64RPFW.Models.Emulation;
+
+public unsafe partial class Mupen64Plus
+{
+
+}
