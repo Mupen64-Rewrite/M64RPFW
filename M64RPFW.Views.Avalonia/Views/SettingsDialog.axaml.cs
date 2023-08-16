647674e313514497f93bad5f9974b5c10a1e063d
Alex <48759429+Aurumaker72@users.noreply.github.com>
2023-08-16 16:03:30 +0200

    Fix settings dialog cancel button not reverting some states

--- M64RPFW.Views.Avalonia/Views/SettingsDialog.axaml.cs
+++ M64RPFW.Views.Avalonia/Views/SettingsDialog.axaml.cs
@@ -1,3 +1,7 @@
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
@@ -7,14 +11,26 @@ namespace M64RPFW.Views.Avalonia.Views;
 
 public partial class SettingsDialog : Window
 {
+    private Dictionary<string, object> _initialSettingsValues = new();
+
     public SettingsDialog()
     {
         AvaloniaXamlLoader.Load(this);
         DataContext = SettingsViewModel.Instance;
         SettingsViewModel.Instance.RequiresRestart = false;
+
+        // duplicate and store all properties from settings vm, so we can restore them if user clicks "Cancel"
+        // this is not very good for performance, since it involves not only reflection, but also a massive dictionary 
+        var props = typeof(SettingsViewModel).GetProperties(BindingFlags.Public | BindingFlags.Instance).Where(prop => prop.SetMethod != null);
+        foreach (var prop in props)
+        {
+            var value = prop.GetValue(SettingsViewModel.Instance);
+            Debug.Assert(value != null);
+            _initialSettingsValues[prop.Name] = value;
+        }
     }
 
-    public SettingsViewModel ViewModel => (SettingsViewModel) DataContext!;
+    public SettingsViewModel ViewModel => (SettingsViewModel)DataContext!;
 
     private void OnApplyClicked(object? sender, RoutedEventArgs e)
     {
@@ -23,8 +39,16 @@ public partial class SettingsDialog : Window
 
     private void OnCancelClicked(object? sender, RoutedEventArgs e)
     {
-        Close();
+        foreach (var pair in _initialSettingsValues)
+        {
+            var property = typeof(SettingsViewModel).GetProperty(pair.Key, BindingFlags.Public | BindingFlags.Instance);
+            Debug.Assert(property != null);
+            property.SetValue(SettingsViewModel.Instance, pair.Value);
+        }
+        ViewModel.NotifyAllPropertiesChanged();
+
         ViewModel.RequiresRestart = false;
+        Close();
     }
 
     private void OnOKClicked(object? sender, RoutedEventArgs e)
