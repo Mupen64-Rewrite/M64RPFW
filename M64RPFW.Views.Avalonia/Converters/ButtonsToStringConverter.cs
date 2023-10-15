5854f842025f75ba5cf074a2d44edceaec91be25
Alex <48759429+Aurumaker72@users.noreply.github.com>
2023-10-15 19:51:47 +0200

    Add rudimentary statusbar with input display (see FIXME)

---
+++ M64RPFW.Views.Avalonia/Converters/ButtonsToStringConverter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+using M64RPFW.Models.Types;
+
+namespace M64RPFW.Views.Avalonia.Converters;
+
+public class ButtonsToStringConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not Mupen64PlusTypes.Buttons buttons)
+            throw new ArgumentException($"Expected {nameof(Mupen64PlusTypes.Buttons)}, got {value?.GetType().FullName}");
+        var result = $"({buttons.JoyX}, {buttons.JoyY}) ";
+
+        if (buttons.BtnMask.HasFlag(Mupen64PlusTypes.ButtonMask.A))
+        {
+            result += "A ";
+        }
+        if (buttons.BtnMask.HasFlag(Mupen64PlusTypes.ButtonMask.B))
+        {
+            result += "B ";
+        }
+        if (buttons.BtnMask.HasFlag(Mupen64PlusTypes.ButtonMask.Start))
+        {
+            result += "S ";
+        }
+        if (buttons.BtnMask.HasFlag(Mupen64PlusTypes.ButtonMask.Z))
+        {
+            result += "Z ";
+        }
+        if (buttons.BtnMask.HasFlag(Mupen64PlusTypes.ButtonMask.L))
+        {
+            result += "L ";
+        }
+        if (buttons.BtnMask.HasFlag(Mupen64PlusTypes.ButtonMask.R))
+        {
+            result += "R ";
+        }
+        if (buttons.BtnMask.HasFlag(Mupen64PlusTypes.ButtonMask.CLeft))
+        {
+            result += "C< ";
+        }
+        if (buttons.BtnMask.HasFlag(Mupen64PlusTypes.ButtonMask.CUp))
+        {
+            result += "C^ ";
+        }
+        if (buttons.BtnMask.HasFlag(Mupen64PlusTypes.ButtonMask.CRight))
+        {
+            result += "C> ";
+        }
+        if (buttons.BtnMask.HasFlag(Mupen64PlusTypes.ButtonMask.CDown))
+        {
+            result += "Cv ";
+        }
+        if (buttons.BtnMask.HasFlag(Mupen64PlusTypes.ButtonMask.DLeft))
+        {
+            result += "D< ";
+        }
+        if (buttons.BtnMask.HasFlag(Mupen64PlusTypes.ButtonMask.DUp))
+        {
+            result += "D^ ";
+        }
+        if (buttons.BtnMask.HasFlag(Mupen64PlusTypes.ButtonMask.DRight))
+        {
+            result += "D> ";
+        }
+        if (buttons.BtnMask.HasFlag(Mupen64PlusTypes.ButtonMask.DDown))
+        {
+            result += "Dv ";
+        }
+        
+        return result;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
