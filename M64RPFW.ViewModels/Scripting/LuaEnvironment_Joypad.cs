15a9fbf2dfa5bef6486b43bba4b493c8b4c9bc40
jgcodes2020 <jackyguo18@hotmail.com>
2023-10-16 22:05:36 -0400

    Complete skeleton implementation of joypad API

--- M64RPFW.ViewModels/Scripting/LuaEnvironment_Joypad.cs
+++ M64RPFW.ViewModels/Scripting/LuaEnvironment_Joypad.cs
@@ -12,7 +12,7 @@ public partial class LuaEnvironment
     private LuaTable GetJoypad(int port)
     {
         var table = _lua.NewUnnamedTable();
-        
+
         // TODO: GetKeys is not implemented fully in core
         var buttons = new Mupen64PlusTypes.Buttons();
 
@@ -32,7 +32,7 @@ public partial class LuaEnvironment
         table["L"] = buttons.BtnMask.HasFlag(Mupen64PlusTypes.ButtonMask.L);
         table["X"] = buttons.JoyX;
         table["Y"] = buttons.JoyY;
-        
+
         return table;
     }
 
@@ -40,5 +40,43 @@ public partial class LuaEnvironment
     private void SetJoypad(int port, LuaTable table)
     {
         // TODO: SetKeys is not implemented fully in core
+
+        var buttons = new Mupen64PlusTypes.Buttons();
+
+        if ((bool) table["right"])
+            buttons.BtnMask |= Mupen64PlusTypes.ButtonMask.DRight;
+        if ((bool) table["left"])
+            buttons.BtnMask |= Mupen64PlusTypes.ButtonMask.DLeft;
+        if ((bool) table["down"])
+            buttons.BtnMask |= Mupen64PlusTypes.ButtonMask.DDown;
+        if ((bool) table["up"])
+            buttons.BtnMask |= Mupen64PlusTypes.ButtonMask.DUp;
+        if ((bool) table["start"])
+            buttons.BtnMask |= Mupen64PlusTypes.ButtonMask.Start;
+        if ((bool) table["Z"])
+            buttons.BtnMask |= Mupen64PlusTypes.ButtonMask.Z;
+        if ((bool) table["B"])
+            buttons.BtnMask |= Mupen64PlusTypes.ButtonMask.B;
+        if ((bool) table["A"])
+            buttons.BtnMask |= Mupen64PlusTypes.ButtonMask.A;
+        if ((bool) table["Cright"])
+            buttons.BtnMask |= Mupen64PlusTypes.ButtonMask.CRight;
+        if ((bool) table["Cleft"])
+            buttons.BtnMask |= Mupen64PlusTypes.ButtonMask.CLeft;
+        if ((bool) table["Cdown"])
+            buttons.BtnMask |= Mupen64PlusTypes.ButtonMask.CDown;
+        if ((bool) table["Cup"])
+            buttons.BtnMask |= Mupen64PlusTypes.ButtonMask.CUp;
+        if ((bool) table["R"])
+            buttons.BtnMask |= Mupen64PlusTypes.ButtonMask.R;
+        if ((bool) table["L"])
+            buttons.BtnMask |= Mupen64PlusTypes.ButtonMask.L;
+
+        // 3 layers of casting needed:
+        // - object -> double
+        // - double -> int (ensure signed conversion)
+        // - int -> sbyte (signed truncation to sbyte)
+        buttons.JoyX = (sbyte) (int) (double) table["X"];
+        buttons.JoyY = (sbyte) (int) (double) table["Y"];
     }
 }
